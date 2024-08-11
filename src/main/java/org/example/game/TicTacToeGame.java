d4fbf2c56875731e279ba7f76da88ddcd00517e7
ruslana <ruslana.prus@ukr.net>
2024-08-11 20:04:13 +0200

    change board to String

--- src/main/java/org/example/game/TicTacToeGame.java
+++ src/main/java/org/example/game/TicTacToeGame.java
@@ -8,9 +8,9 @@ public class TicTacToeGame {
     private static final char COMPUTER_MARK = 'O';
     private static final int BOARD_SIZE = 16;
     private static final int ROW_SIZE = (int) Math.sqrt(BOARD_SIZE);
-    private static final char EMPTY_CELL = ' ';
+    private static final String EMPTY_CELL = " ";
 
-    private final char[] board = new char[BOARD_SIZE];
+    private final String[] board = new String[BOARD_SIZE];
     private final Scanner scanner = new Scanner(System.in);
     private final char[] players = {USER_MARK, COMPUTER_MARK};
 
@@ -21,7 +21,7 @@ public class TicTacToeGame {
     }
 
     private void initializeNumberedBoard() {
-        IntStream.range(0, BOARD_SIZE).forEach(i -> board[i] = (char) ('1' + i));
+        IntStream.range(0, BOARD_SIZE).forEach(i -> board[i] = String.valueOf(i + 1));
     }
 
     public void play() {
@@ -54,14 +54,14 @@ public class TicTacToeGame {
     public void printBoard() {
         System.out.println();
         for (int i = 0; i < BOARD_SIZE; i++) {
-            System.out.print(" " + board[i]);
+            System.out.printf("%2s", board[i]);
             if ((i + 1) % ROW_SIZE == 0) {
                 System.out.println();
                 if (i < BOARD_SIZE - 1) {
-                    System.out.println("--".repeat(ROW_SIZE * 2 - 1));
+                    System.out.println("----".repeat(ROW_SIZE));
                 }
             } else {
-                System.out.print(" |");
+                System.out.print(" | ");
             }
         }
         System.out.println();
@@ -77,8 +77,8 @@ public class TicTacToeGame {
         System.out.println(message + "\nCreated by Shreyas Saha. Thanks for playing!");
     }
 
-    private void makeMove(char player){
-        if(player == USER_MARK){
+    private void makeMove(char player) {
+        if (player == USER_MARK) {
             makeUserMove();
         } else makeComputerMove();
     }
@@ -87,8 +87,8 @@ public class TicTacToeGame {
         byte move;
         do {
             move = getUserMove();
-        } while (board[move - 1] == USER_MARK || board[move - 1] == COMPUTER_MARK);
-        board[move - 1] = USER_MARK;
+        } while (board[move - 1].equals(String.valueOf(USER_MARK)) || board[move - 1].equals(String.valueOf(COMPUTER_MARK)));
+        board[move - 1] = String.valueOf(USER_MARK);
     }
 
     private byte getUserMove() {
@@ -107,28 +107,28 @@ public class TicTacToeGame {
         byte move;
         do {
             move = (byte) ((Math.random() * BOARD_SIZE) + 1);
-        } while (board[move - 1] == USER_MARK || board[move - 1] == COMPUTER_MARK);
-        board[move - 1] = COMPUTER_MARK;
+        } while (board[move - 1].equals(String.valueOf(USER_MARK)) || board[move - 1].equals(String.valueOf(COMPUTER_MARK)));
+        board[move - 1] = String.valueOf(COMPUTER_MARK);
     }
 
     private boolean checkWinner(char player) {
         for (int i = 0; i < BOARD_SIZE; i += ROW_SIZE) {
-            if (IntStream.range(i, i + ROW_SIZE).allMatch(j -> board[j] == player)) {
+            if (IntStream.range(i, i + ROW_SIZE).allMatch(j -> board[j].equals(String.valueOf(player)))) {
                 return true;
             }
         }
 
         for (int i = 0; i < ROW_SIZE; i++) {
             int finalI = i;
-            if (IntStream.range(0, ROW_SIZE).allMatch(j -> board[finalI + j * ROW_SIZE] == player)) {
+            if (IntStream.range(0, ROW_SIZE).allMatch(j -> board[finalI + j * ROW_SIZE].equals(String.valueOf(player)))) {
                 return true;
             }
         }
 
-        if (IntStream.range(0, ROW_SIZE).allMatch(i -> board[i * (ROW_SIZE + 1)] == player)) {
+        if (IntStream.range(0, ROW_SIZE).allMatch(i -> board[i * (ROW_SIZE + 1)].equals(String.valueOf(player)))) {
             return true;
         }
-        if (IntStream.range(1, ROW_SIZE + 1).allMatch(i -> board[i * (ROW_SIZE - 1)] == player)) {
+        if (IntStream.range(1, ROW_SIZE + 1).allMatch(i -> board[i * (ROW_SIZE - 1)].equals(String.valueOf(player)))) {
             return true;
         }
 
@@ -136,6 +136,6 @@ public class TicTacToeGame {
     }
 
     private boolean checkDraw() {
-        return IntStream.range(0, BOARD_SIZE).noneMatch(i -> board[i] == EMPTY_CELL);
+        return IntStream.range(0, BOARD_SIZE).noneMatch(i -> board[i].equals(EMPTY_CELL));
     }
 }
